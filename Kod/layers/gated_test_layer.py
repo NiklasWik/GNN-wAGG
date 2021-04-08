9763d6e999be7d1231a78a2b25d2b2b500038895
Andreas Ström <andreascstrom@gmail.com>
2021-04-08 18:35:50 +0200

    fix fire gated

--- Kod/layers/gated_test_layer.py
+++ Kod/layers/gated_test_layer.py
@@ -78,9 +78,9 @@ class GatedTestLayer(nn.Module):
         g.edata['e'] = g.edata['DEh'] + g.edata['Ce']
         g.edata['sigma'] = torch.sigmoid(g.edata['e']) 
         g.update_all(fn.copy_e('sigma', 'm'), fn.sum('m', 'sum_sigma')) 
-        g.ndata['eee'] = g.ndata['Bh'] / (g.ndata['sum_sigma'] + 1e-6)
-        g.update_all(fn.u_mul_e('sigma', 'eee', 'm'), self._reducer) 
-        g.ndata['h'] = g.ndata['Ah'] + g.ndata['sum_sigma_h'] 
+        g.ndata['eee'] = g.ndata['Bh']
+        g.update_all(fn.u_mul_e('eee', 'sigma', 'm'), self._reducer) 
+        g.ndata['h'] = g.ndata['Ah'] + g.ndata['sum_sigma_h']  / (g.ndata['sum_sigma'] + 1e-6)
 
         #h, e = self.update_all_p_norm(g)
         
