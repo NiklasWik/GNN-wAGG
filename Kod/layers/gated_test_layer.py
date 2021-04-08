afa529360dc2b7db1a3c5ad2c8aa5a5dc09ef6cb
Andreas Ström <andreascstrom@gmail.com>
2021-04-08 18:01:01 +0200

    fix fire gated

--- Kod/layers/gated_test_layer.py
+++ Kod/layers/gated_test_layer.py
@@ -58,7 +58,7 @@ class GatedTestLayer(nn.Module):
         p = torch.clamp(self.P,1,100)
         #h = torch.abs(nodes.mailbox['m']).pow(P)
         h = torch.exp(nodes.mailbox['m'])
-        alpha,_ = torch.max(h, dim=0)
+        alpha = torch.max(h)
         print(alpha)
         h = (h/alpha).pow(p)
         return {'sum_sigma_h': (torch.sum(h, dim=1).pow(1/p))*alpha}
