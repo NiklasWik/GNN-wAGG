02733203a40b283f6368707870bd12d8a65d6a78
NiklasWik <62885387+NiklasWik@users.noreply.github.com>
2021-04-09 15:43:52 +0200

    Update gated_test_layer.py

--- Kod/layers/gated_test_layer.py
+++ Kod/layers/gated_test_layer.py
@@ -71,8 +71,8 @@ class GatedTestLayer(nn.Module):
         h = torch.abs((h/alpha)).pow(p)
         if torch.isnan(h).any():
             print("rad 71")
-            print(htemp.detach().numpy()[torch.isnan(h).detach().numpy()])
-            print(p.detach().numpy()[torch.isnan(h).detach().numpy()])
+            #print(htemp.detach().numpy()[torch.isnan(h).detach().numpy()])
+            #print(p.detach().numpy()[torch.isnan(h).detach().numpy()])
         if torch.isnan(p).any():
             print("p-nan")
 
