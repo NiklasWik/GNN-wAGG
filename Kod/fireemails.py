1e7cb28303bdc84dd61a22cbb960e6ca4917b4f8
Andreas Ström <andreascstrom@gmail.com>
2021-04-10 19:20:06 +0200

    :rocket: added try/except

--- Kod/fireemails.py
+++ Kod/fireemails.py
@@ -9,7 +9,6 @@ from email import encoders
 
 def send_mail(send_to, subject, message, files=[], password=''):
     """Compose and send email with provided info and attachments.
-
     Args:
         send_to (list[str]): to name(s)
         subject (str): message title
@@ -17,13 +16,11 @@ def send_mail(send_to, subject, message, files=[], password=''):
         files (list[str]): list of file paths to be attached to email
         password (str): server auth password
     """
-    send_from = 'mortssaerdna2@gmail.com'
+    username = 'mortssaerdna2@gmail.com'
     server = 'smtp.gmail.com'
     port = 587
-    username = send_from
-    use_tls = True
     msg = MIMEMultipart()
-    msg['From'] = send_from
+    msg['From'] = username
     msg['To'] = COMMASPACE.join(send_to)
     msg['Date'] = formatdate(localtime=True)
     msg['Subject'] = subject
@@ -31,18 +28,20 @@ def send_mail(send_to, subject, message, files=[], password=''):
     msg.attach(MIMEText(message))
 
     for path in files:
-        part = MIMEBase('application', "octet-stream")
-        with open(path, 'rb') as file:
-            part.set_payload(file.read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition',
-                        'attachment; filename="{}"'.format(Path(path).name))
-        msg.attach(part)
+        try:
+            part = MIMEBase('application', "octet-stream")
+            with open(path, 'rb') as file:
+                part.set_payload(file.read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition',
+                            'attachment; filename="{}"'.format(Path(path).name))
+            msg.attach(part)
+        except:
+            print('filename="{}" not found'.format(Path(path).name))
 
     smtp = smtplib.SMTP(server, port)
-    if use_tls:
-        smtp.starttls()
+    smtp.starttls()
     smtp.login(username, password)
-    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.sendmail(username, send_to, msg.as_string())
     smtp.quit()
   
