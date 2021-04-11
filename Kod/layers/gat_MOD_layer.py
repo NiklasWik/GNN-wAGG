669f9fc4352ca25594f85ec42de18b20cc2ff74f
NiklasWik <62885387+NiklasWik@users.noreply.github.com>
2021-04-11 14:35:01 +0200

    Update gat_MOD_layer.py

--- Kod/layers/gat_MOD_layer.py
+++ Kod/layers/gat_MOD_layer.py
@@ -110,7 +110,6 @@ class CustomGATHeadLayer(nn.Module):
         return {'h': h}
 
     def forward(self, g, h, e):
-        print(h)
         z = self.fc(h)
         g.ndata['z'] = z
         g.apply_edges(self.edge_attention)
