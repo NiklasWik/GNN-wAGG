f5515ab3c1ac0f34a811caadb837eb672da56fc9
NiklasWik <62885387+NiklasWik@users.noreply.github.com>
2021-04-15 16:44:01 +0200

    Update graphsage_MOD_layer.py

--- Kod/layers/graphsage_MOD_layer.py
+++ Kod/layers/graphsage_MOD_layer.py
@@ -76,7 +76,6 @@ class GraphSageLayer(nn.Module):
         msg = torch.abs(nodes.mailbox['m'])
         fsum = torch.sum(torch.sigmoid(w*msg+self.b), dim=1)
         sig_in = torch.clamp(fsum/torch.max(fsum), 0.000001, 0.9999999)
-        print(torch.min(sig_in))
         out_h = (torch.log(sig_in/(1-sig_in))-self.b)/w
         return {'c': out_h}
 
