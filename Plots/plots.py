365476e67e830ecd8709fd6cabddf4e1afa1a4cd
Andreas Ström <andreascstrom@gmail.com>
2021-04-16 17:02:17 +0200

    Even more straight fire plots :rocket:

--- Plots/plots.py
+++ Plots/plots.py
@@ -3,22 +3,22 @@ import matplotlib.pyplot as plt
 sns.set_theme(style="ticks", color_codes=True)
 import pandas as pd 
 from matplotlib import pyplot as plt
-import os
-here = os.path.dirname(os.path.abspath(__file__))
-res = pd.read_csv(os.path.join(here, "Resultat.csv"), sep = ',', decimal=',')
+sheet_id = "1T66TbM4FE8WcSFyaXqV5FlY90DCyWp81"
+sheet_name = "EasyExport"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+res = pd.read_csv(url, sep = ',', decimal=',')
 res = res.replace(to_replace="pnorm", value="p-norm")
 res = res.replace(to_replace="fsig", value="f-sum sigmoid")
 res = res.replace(to_replace="fleaky", value="f-sum leaky")
-lab = list(res.head())
-pattern = res[res.DATA=="PATTERN"]
-cluster = res[res.DATA=="CLUSTER"]
+datasets = [res[res.DATA=="PATTERN"], res[res.DATA=="CLUSTER"]]
 colors = ["#3498DB","#e74c3c","#34495e","#2ecc71"]
 yvec = ["TEST_ACC", "TRAIN_ACC"]
 
 for _,yuse in enumerate(yvec):
-    g = sns.catplot(x="GNN", y=yuse, hue="AGG", kind="bar", data=pattern, ci="sd", capsize=0.1, palette=colors)
-    g.set(ylim=(min(pattern[yuse])-(max(res[yuse])-min(pattern[yuse]))*.05, max(pattern[yuse])+(max(res[yuse])-min(pattern[yuse]))*.1))
-    g.set_xlabels("Network")
-    g.set_ylabels("Accuracy (%)")
-    plt.title(yuse[:-4].title()+"ing accuracy" + " (PATTERN)")
+    for _,dataset in enumerate(datasets):
+        g = sns.catplot(x="GNN", y=yuse, hue="AGG", kind="bar", data=dataset, ci="sd", capsize=0.1, palette=colors)
+        g.set(ylim=(min(dataset[yuse])-(max(res[yuse])-min(dataset[yuse]))*.05, max(dataset[yuse])+(max(res[yuse])-min(dataset[yuse]))*.1))
+        g.set_xlabels("Network")
+        g.set_ylabels("Accuracy (%)")
+        plt.title(yuse[:-4].title()+"ing accuracy, (" + dataset.iloc[0]['DATA'] +")")
 plt.show()
