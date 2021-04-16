9bf9bb4f3d225c6fc89132c21b36307369fb5ff6
Andreas Ström <andreascstrom@gmail.com>
2021-04-16 14:25:23 +0200

    more absolute fire plots :rocket:

--- Plots/plots.py
+++ Plots/plots.py
@@ -7,16 +7,16 @@ import os
 here = os.path.dirname(os.path.abspath(__file__))
 res = pd.read_csv(os.path.join(here, "Resultat.csv"), sep = ',', decimal=',')
 res = res.replace(to_replace="pnorm", value="p-norm")
-res = res.replace(to_replace="pnorm", value="p-norm")
+res = res.replace(to_replace="fsig", value="f-sum sigmoid")
+res = res.replace(to_replace="fleaky", value="f-sum leaky")
 lab = list(res.head())
 pattern = res[res.DATA=="PATTERN"]
 cluster = res[res.DATA=="CLUSTER"]
 colors = ["#3498DB","#e74c3c","#34495e","#2ecc71"]
-sns.set_palette(colors)
 yvec = ["TEST_ACC", "TRAIN_ACC"]
 
 for _,yuse in enumerate(yvec):
-    g = sns.catplot(x="GNN", y=yuse, hue="AGG", kind="bar", data=pattern, ci="sd", capsize=0.1)
+    g = sns.catplot(x="GNN", y=yuse, hue="AGG", kind="bar", data=pattern, ci="sd", capsize=0.1, palette=colors)
     g.set(ylim=(min(pattern[yuse])-(max(res[yuse])-min(pattern[yuse]))*.05, max(pattern[yuse])+(max(res[yuse])-min(pattern[yuse]))*.1))
     g.set_xlabels("Network")
     g.set_ylabels("Accuracy (%)")
