3e19315fee64a0a8288f69f558f06d687835731a
Andreas Ström <andreascstrom@gmail.com>
2021-04-16 14:05:32 +0200

    absolute fire plots :rocket:

--- Plots/plots.py
+++ Plots/plots.py
@@ -1,33 +1,24 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 14 15:57:48 2021
-
-@author: juliasolhed
-"""
-
 import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set_theme(style="ticks", color_codes=True)
-
 import pandas as pd 
 from matplotlib import pyplot as plt
-dataTrain = pd.read_csv("trainPattern.csv")
-dataTest = pd.read_csv("testPattern.csv")
-
-#sns.catplot(x="GNN",y="Train_accuracy",hue='A',kind="bar", palette="pastel", edgecolor=".6",data=da)
-#sns.catplot(x="GNN", y="AA", hue="A", kind="bar", data=da)
-
+import os
+here = os.path.dirname(os.path.abspath(__file__))
+res = pd.read_csv(os.path.join(here, "Resultat.csv"), sep = ',', decimal=',')
+res = res.replace(to_replace="pnorm", value="p-norm")
+res = res.replace(to_replace="pnorm", value="p-norm")
+lab = list(res.head())
+pattern = res[res.DATA=="PATTERN"]
+cluster = res[res.DATA=="CLUSTER"]
 colors = ["#3498DB","#e74c3c","#34495e","#2ecc71"]
 sns.set_palette(colors)
-g = sns.catplot(x="GNN", y="Test_Acc", hue="Agg", kind="bar", data=dataTest)
-g.set(ylim=(85, 86))
-plt.title("Test accuracy (PATTERN)")
-plt.show(g)
+yvec = ["TEST_ACC", "TRAIN_ACC"]
 
-g = sns.catplot(x="GNN", y="Train_Acc", hue="Agg", kind="bar", data=dataTrain)
-g.set(ylim=(85.5, 87))
-plt.title("Test accuracy (PATTERN)")
-plt.show(g)
-
-#g= sns.catplot(x="GNN", y="Train", hue="Agg", kind="swarm",aspect=1, data=da, palette="Spectral")
+for _,yuse in enumerate(yvec):
+    g = sns.catplot(x="GNN", y=yuse, hue="AGG", kind="bar", data=pattern, ci="sd", capsize=0.1)
+    g.set(ylim=(min(pattern[yuse])-(max(res[yuse])-min(pattern[yuse]))*.05, max(pattern[yuse])+(max(res[yuse])-min(pattern[yuse]))*.1))
+    g.set_xlabels("Network")
+    g.set_ylabels("Accuracy (%)")
+    plt.title(yuse[:-4].title()+"ing accuracy" + " (PATTERN)")
+plt.show()
