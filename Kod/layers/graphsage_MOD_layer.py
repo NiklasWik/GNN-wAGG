db1d9cca2cd642f4cef36d1687b7fb6642bd6c0f
NiklasWik <62885387+NiklasWik@users.noreply.github.com>
2021-04-20 21:35:12 +0200

    Update graphsage_MOD_layer.py

--- Kod/layers/graphsage_MOD_layer.py
+++ Kod/layers/graphsage_MOD_layer.py
@@ -44,8 +44,8 @@ class GraphSageLayer(nn.Module):
         elif aggregator_type == "planar_sig":
             print("planar_sig")
             self._reducer = self.reduce_planar
-            self.w = nn.Parameter(torch.rand(out_dim)-3)
-            self.b = nn.Parameter((torch.rand(out_dim)*0.1-1/10))
+            self.w = nn.Parameter(torch.rand(in_feats)-3)
+            self.b = nn.Parameter((torch.rand(in_feats)*0.1-1/10))
         else:
             self.aggregator = MeanAggregator()
             print("DU KÖR MED MEAN??? DET HÄR FUNKAR INTE")
