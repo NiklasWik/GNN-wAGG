d204ad930aeab88770863a5479c57f6db02fb812
andreasstrom <andreascstrom@gmail.com>
2021-04-20 16:00:41 +0200

    testing planar GAT :rooster:

--- Kod/layers/gat_MOD_layer.py
+++ Kod/layers/gat_MOD_layer.py
@@ -117,7 +117,8 @@ class CustomGATHeadLayer(nn.Module):
         fsum = torch.sum(torch.sigmoid(w*msg+self.b), dim=1)
         #print(fsum)
         #sig_in = torch.clamp(fsum/torch.max(fsum), 0.000001, 0.9999999)
-        sig_in = torch.clamp(fsum, 0.000001, 0.9999999)
+        maxn = torch.max(fsum)
+        sig_in = .99*fsum/maxn+.05
         out_h = (torch.log(sig_in/(1-sig_in))-self.b)/w
         return {'h': out_h}
         
