45f698b51ae2933758e7794dea635cc8b28f6c26
andreasstrom <andreascstrom@gmail.com>
2021-05-11 14:21:01 +0200

    sig inits check

--- Kod/layers/graphsage_MOD_layer.py
+++ Kod/layers/graphsage_MOD_layer.py
@@ -45,7 +45,7 @@ class GraphSageLayer(nn.Module):
             print("planar_sig")
             self._reducer = self.reduce_planar
             self.w = nn.Parameter(torch.rand(in_feats)-1)
-            self.b = nn.Parameter((torch.rand(in_feats)*1-6.5))
+            self.b = nn.Parameter((torch.rand(in_feats)*1-10))
         elif aggregator_type == "planar_tanh":
             self._reducer = self.reduce_tanh
             self.w = nn.Parameter(torch.rand(in_feats)-4.5)
