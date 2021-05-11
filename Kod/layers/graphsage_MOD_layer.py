153c38f8b9e73bb9c8cb8e2235090f8e87d69071
andreasstrom <andreascstrom@gmail.com>
2021-05-11 14:17:53 +0200

    sig inits check

--- Kod/layers/graphsage_MOD_layer.py
+++ Kod/layers/graphsage_MOD_layer.py
@@ -81,6 +81,8 @@ class GraphSageLayer(nn.Module):
         msg = nodes.mailbox['m']
         fsum = torch.sum(torch.sigmoid(w*msg+self.b), dim=1)
         #sig_in = torch.clamp(fsum/torch.max(fsum), 0.000001, 0.9999999)
+        print(torch.max(fsum))
+        print(torch.min(fsum))
         sig_in = torch.clamp(fsum, 0.000001, 0.9999999)
         out_h = (torch.log(sig_in/(1-sig_in))-self.b)/w
         return {'c': out_h}
