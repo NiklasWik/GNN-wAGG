bb55725e49cf4746f3ff0f0d8c05ea996b4aff5b
andreasstrom <andreascstrom@gmail.com>
2021-05-17 19:21:48 +0200

    start of fire table generation

--- Plots/plots.py
+++ Plots/plots.py
@@ -14,7 +14,7 @@ res = res.replace(to_replace="planar_tanh", value="planar$_{\mathrm{tanh}}$")
 datasets = [res[res.DATA=="PATTERN"], res[res.DATA=="CLUSTER"]]
 colors = ["#3498DB","#e74c3c","#FFD548","#2ecc71"]
 
-if True:
+if False:
     yvec = ["TEST_ACC", "TRAIN_ACC"]
     for _,yuse in enumerate(yvec):
         for _,dataset in enumerate(datasets):
@@ -26,7 +26,7 @@ if True:
             plt.savefig(str(pathlib.Path(__file__).parent.absolute())+"//"+yuse[:-4].replace(" ", "")+"-" + dataset.iloc[0]['DATA']+".eps", bbox_inches="tight", format="eps")
     plt.show()
 
-if True:
+if False:
     yvec = ["EPOCHS", "EPOCH TIME (s)", "TOTAL TIME (h)"]
     ylabs = ["Number of epochs (#)", "Time per epoch (s)", "Total training time (h)"]
     for i,yuse in enumerate(yvec):
@@ -38,3 +38,19 @@ if True:
             plt.title(yuse[0:10].title()+", (" + dataset.iloc[0]['DATA'] +")")
             plt.savefig(str(pathlib.Path(__file__).parent.absolute())+"//"+yuse[0:10].replace(" ", "")+"-" + dataset.iloc[0]['DATA']+".eps", bbox_inches="tight", format="eps")
     plt.show()
+
+if True:
+    gnns = ["GATED", "SAGE", "GAT", "GIN"]
+    dset = ["PATTERN", "CLUSTER"]
+    for j,data in enumerate(datasets):
+        data = data.replace(to_replace="planar$_{\mathrm{sig}}$", value="planar_sig")
+        data = data.replace(to_replace="planar$_{\mathrm{tanh}}$", value="planar_tanh")
+        
+        for i,df in enumerate([data[data.GNN==x] for x in gnns]):
+            df = data.groupby(['AGG'], as_index=False).agg({'Params':'mean', 'TEST_ACC':['mean','std'], 'TRAIN_ACC':['mean','std'], 'EPOCHS':'mean', 'EPOCH TIME (s)':'mean', 'TOTAL TIME (h)':'mean'})
+            print(df)
+            for asd in ["TEST_ACC", "TRAIN_ACC"]:
+                print(df[asd])
+                df[asd] = df[asd]['mean'].astype(str)+'±'+df[asd]['std'].astype(str)
+            f = open(str(pathlib.Path(__file__).parent.absolute())+'//'+gnns[i]+'_'+dset[j]+'_table.txt','w')
+            f.write(df.to_latex(index=False))
