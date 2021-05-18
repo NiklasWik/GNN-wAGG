539e160f23f6ce4b1a5dfce02716d6b536aeacaf
andreasstrom <andreascstrom@gmail.com>
2021-05-18 17:00:10 +0200

    fire tables huh :rooster:

--- Plots/plots.py
+++ Plots/plots.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pathlib
 sns.set_theme(style="ticks", color_codes=True)
 import pandas as pd 
+import numpy as np
 from matplotlib import pyplot as plt
 sheet_id = "1T66TbM4FE8WcSFyaXqV5FlY90DCyWp81"
 sheet_name = "EasyExport"
@@ -43,14 +44,24 @@ if True:
     gnns = ["GATED", "SAGE", "GAT", "GIN"]
     dset = ["PATTERN", "CLUSTER"]
     for j,data in enumerate(datasets):
-        data = data.replace(to_replace="planar$_{\mathrm{sig}}$", value="planar_sig")
-        data = data.replace(to_replace="planar$_{\mathrm{tanh}}$", value="planar_tanh")
-        
+        pd.options.display.float_format = '{:,.3f}'.format
         for i,df in enumerate([data[data.GNN==x] for x in gnns]):
-            df = data.groupby(['AGG'], as_index=False).agg({'Params':'mean', 'TEST_ACC':['mean','std'], 'TRAIN_ACC':['mean','std'], 'EPOCHS':'mean', 'EPOCH TIME (s)':'mean', 'TOTAL TIME (h)':'mean'})
-            print(df)
+            df = data.groupby(['AGG'], as_index=False).agg({'Layers':'mean', 'Params':'mean', 'TEST_ACC':['mean','std'], 'TRAIN_ACC':['mean','std'], 'EPOCHS':'mean', 'EPOCH TIME (s)':'mean', 'TOTAL TIME (h)':'mean'})
             for asd in ["TEST_ACC", "TRAIN_ACC"]:
-                print(df[asd])
-                df[asd] = df[asd]['mean'].astype(str)+'±'+df[asd]['std'].astype(str)
-            f = open(str(pathlib.Path(__file__).parent.absolute())+'//'+gnns[i]+'_'+dset[j]+'_table.txt','w')
-            f.write(df.to_latex(index=False))
+                df[asd] = df[asd]['mean'].round(3).astype(str)+'±'+df[asd]['std'].round(3).astype(str)
+            df = df.drop('std', axis=1, level=1)
+            df.columns = df.columns.droplevel(1)
+            df.Params = df.Params.round(0).astype(int)
+            df['EPOCHS'] = df['EPOCHS'].round(2).astype(str)
+            df['EPOCH TIME (s)'] = df['EPOCH TIME (s)'].round(2).astype(str)+'s/'+df['TOTAL TIME (h)'].round(2).astype(str)+'h'
+            df = df.drop('TOTAL TIME (h)', axis = 1)
+            df.AGG = df.AGG.astype(str)
+            df.columns = ["\textbf{Agg. func.}", "\emph{L}", "\textbf{#Params}", "\textbf{Test Acc. ± s.d.}", "\textbf{Train Acc. ± s.d.}", "\textbf{#Epochs}", "\textbf{Epoch/Total}"] 
+            df1 = df[["\textbf{Agg. func.}", "\emph{L}"]]
+            df2 = df[["\textbf{#Params}", "\textbf{Test Acc. ± s.d.}", "\textbf{Train Acc. ± s.d.}", "\textbf{#Epochs}", "\textbf{Epoch/Total}"]]
+            
+            with open(str(pathlib.Path(__file__).parent.absolute())+'//'+gnns[i]+'_'+dset[j]+'_table.tex','w') as f:
+                f.write(df2.to_latex(index=False, escape=False, column_format="|lllll"))
+
+    with open(str(pathlib.Path(__file__).parent.absolute())+'//init_table.tex','w') as f:
+        f.write(df1.to_latex(index=False, escape=False, column_format="lr"))
